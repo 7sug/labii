27bf1068a666f61a06ae4dbc411b695780331c9a
Vlad Stepanchev <55149963+7sug@users.noreply.github.com>
2020-06-20 16:39:17 +0300

    Update быстрая.c

--- labsVol/сортировки/быстрая.c
+++ labsVol/сортировки/быстрая.c
@@ -1,45 +1,79 @@
 #include <stdio.h>
+
 #include <omp.h>
 #include <stdlib.h>
 
-double getShellSortTime(int n);
-void ShellSort(int arr[], int n)
-{
-	int step, i, j, tmp;
+double getQuickSortTime(int n);
 
-	for (step = n / 2; step > 0; step /= 2)
-		for (i = step; i < n; i++)
-			for (j = i - step; j >= 0 && arr[j] > arr[j + step]; j -= step)
-			{
-				tmp = arr[j];
-				arr[j] = arr[j + step];
-				arr[j + step] = tmp;
-			}
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
+
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high- 1; j++)
+    {
+
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int* arr, int low, int high)
+{
+    if (low < high)
+    {
+
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("n");
+}
+
 
 int main()
 {
-	for (int n = 100000; n <= 1000000; n += 100000)
-	{
-		printf("%d;%lf\n", n, getShellSortTime(n));
-	}
+    for (int n = 100000; n <= 1000000; n+=100000)
+    {
+        printf("%d;%lf\n", n, getQuickSortTime(n));
+    }
 
-	return 0;
+    return 0;
 }
-double getShellSortTime(int n)
-{
-	int *data;
+double getQuickSortTime(int n) {
+    int *data;
 
-	data = (int *)malloc(n * sizeof(int));
-	for (int i = 0; i < n; i++)
-		data[i] = rand();
+    data = (int *) malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        data[i] = rand();
 
-	double start_time = omp_get_wtime();
+    double start_time = omp_get_wtime();
 
-	ShellSort(data, n);
-	double stop_time = omp_get_wtime();
+    quickSort(data,0,n-1);
+    double stop_time = omp_get_wtime();
 
-	free(data);
+    free(data);
 
-	return stop_time - start_time;
+    return stop_time - start_time;
 }
